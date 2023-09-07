2b52e0fdb4e7526ceb885f7bb8652d6de36c71da
Bobby <8290615@qq.com>
2023-09-07 14:17:09 +0800

    feat: update

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,2 +1,3 @@
 // component exports
 export * from './alert';
+export * from './number-input';
---
+++ packages/react/src/number-input/index.ts
@@ -0,0 +1,2 @@
+export { useNumberInput } from './use-number-input';
+export type { UseNumberInputProps, UseNumberInputReturn } from './use-number-input';
