8afd0e2cf63b4652426da541033526a9b076075f
Bobby <8290615@qq.com>
2023-09-12 00:22:22 +0800

    feat: add Divider component

---
+++ packages/react/src/divider/divider.tsx
@@ -0,0 +1,56 @@
+import { divider } from '@alice-ui/theme';
+import {
+  ElementType,
+  ForwardedRef,
+  HTMLAttributes,
+  createContext,
+  forwardRef,
+  useMemo,
+} from 'react';
+import { SeparatorProps, useSeparator } from 'react-aria';
+import { ContextValue, useContextProps } from '../_util/utils';
+
+export interface DividerProps extends HTMLAttributes<HTMLHRElement>, SeparatorProps {
+  elementType?: string;
+}
+
+export const DividerContext = createContext<ContextValue<DividerProps, HTMLHRElement>>({});
+
+function Divider(props: DividerProps, ref: ForwardedRef<HTMLHRElement>) {
+  [props, ref] = useContextProps(props, ref, DividerContext);
+  let { elementType, className, orientation, ...otherProps } = props;
+
+  let Component = (elementType || 'hr') as ElementType;
+
+  if (Component === 'hr' && orientation === 'vertical') {
+    Component = 'div';
+  }
+
+  const { separatorProps } = useSeparator({
+    elementType: typeof Component === 'string' ? Component : 'hr',
+    orientation,
+  });
+
+  const styles = useMemo(
+    () =>
+      divider({
+        orientation,
+        className,
+      }),
+    [orientation, className],
+  );
+
+  return (
+    <Component
+      className={styles}
+      role="separator"
+      data-orientation={orientation}
+      {...separatorProps}
+      {...otherProps}
+      ref={ref}
+    />
+  );
+}
+
+const _Divider = forwardRef(Divider);
+export { _Divider as Divider };
---
+++ packages/theme/src/components/divider.ts
@@ -0,0 +1,29 @@
+import type { VariantProps } from 'tailwind-variants';
+
+import { tv } from '../utils/tv';
+
+/**
+ * Divider wrapper **Tailwind Variants** component
+ *
+ * @example
+ *
+ * const styles = divider()
+ *
+ * <span className={styles} />
+ */
+const divider = tv({
+  base: 'shrink-0 bg-divider border-none',
+  variants: {
+    orientation: {
+      horizontal: 'w-full h-divider',
+      vertical: 'h-full w-divider',
+    },
+  },
+  defaultVariants: {
+    orientation: 'horizontal',
+  },
+});
+
+export type DividerVariantProps = VariantProps<typeof divider>;
+
+export { divider };
