105cc106ffe3761ec785e738f1eb3a58732f14dd
Bobby <8290615@qq.com>
2023-12-04 21:03:20 +0800

    fix: export useDisclosure type

--- packages/react/src/button/index.ts
+++ packages/react/src/button/index.ts
@@ -9,8 +9,5 @@ export type { ButtonProps } from './button';
 export type { CloseButtonProps } from './close-button';
 export type { IconButtonProps } from './icon-button';
 
-// export context
-export { ButtonContext } from 'react-aria-components';
-
 // export component
 export { Button, CloseButton, IconButton };
