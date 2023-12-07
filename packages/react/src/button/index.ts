90ea2d55d791ac86afa1d6a28a6839cf65ecacd9
Bobby <8290615@qq.com>
2023-12-07 14:23:08 +0800

    fix: update close button

--- packages/react/src/button/index.ts
+++ packages/react/src/button/index.ts
@@ -6,7 +6,6 @@ import { IconButton } from './icon-button';
 
 // export types
 export type { ButtonProps } from './button';
-export type { CloseButtonProps } from './close-button';
 export type { IconButtonProps } from './icon-button';
 
 // export component
