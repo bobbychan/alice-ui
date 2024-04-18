2c49261c7951984ca2cfe9b2955e93a6c56dc946
Bobby <8290615@qq.com>
2024-04-18 20:42:00 +0800

    fix: update

--- packages/theme/src/components/drawer.ts
+++ packages/theme/src/components/drawer.ts
@@ -33,7 +33,7 @@ const drawer = tv({
     backdrop: ['z-50', 'fixed', 'inset-0', 'w-screen', 'h-screen'],
     dialog: ['outline-none', 'flex', 'flex-col', 'overflow-hidden', 'p-6', 'h-full', 'max-h-dvh'],
     header: ['text-lg', 'font-semibold'],
-    body: ['flex', 'flex-1', 'flex-col', 'gap-3', 'py-4', 'overflow-y-auto'],
+    body: ['flex-1', 'py-4', 'overflow-y-auto'],
     footer: ['flex', 'flex-row', 'gap-2', 'justify-end', 'mt-2'],
     closeButton: ['absolute', 'top-2', 'right-2'],
   },
