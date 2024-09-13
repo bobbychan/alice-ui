a3ed018c5137650c1433f35692bee37b0b0a732a
Bobby <8290615@qq.com>
2024-09-13 16:26:02 +0800

    feat: add Intl.ListFormat polyfill

---
+++ .changeset/unlucky-onions-travel.md
@@ -0,0 +1,5 @@
+---
+'@alice-ui/react': patch
+---
+
+add Intl.ListFormat polyfill
--- packages/react/src/modal/index.ts
+++ packages/react/src/modal/index.ts
@@ -4,9 +4,6 @@ import { ModalCloseButton } from './modal-close-button';
 import { ModalContent } from './modal-content';
 import { ModalFooter } from './modal-footer';
 import { ModalHeader } from './modal-header';
-// https://github.com/adobe/react-spectrum/issues/5720
-import '@formatjs/intl-listformat/locale-data/en';
-import '@formatjs/intl-listformat/polyfill';
 
 // export types
 export type { ModalProps } from './modal';
