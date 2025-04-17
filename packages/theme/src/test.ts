f59ad2e465c9d76b692484cf60b99a38ed32b0d6
Bobby <8290615@qq.com>
2025-04-18 00:13:23 +0800

    feat: update theme plugin

--- packages/theme/src/test.ts
+++ packages/theme/src/test.ts
@@ -1,24 +1,26 @@
-import { kebabCase, mapKeys, omit } from '@heroui/shared-utils';
+/**
+ * Based on tw-colors by L-Blondy
+ * @see https://github.com/L-Blondy/tw-colors
+ */
+
 import Color from 'color';
+// @ts-ignore
+import { kebabCase, mapKeys, omit } from '@heroui/shared-utils';
 import deepMerge from 'deepmerge';
 import plugin from 'tailwindcss/plugin.js';
 
 import { animations } from './animations';
 import { commonColors, semanticColors } from './colors';
 import { darkLayout, defaultLayout, lightLayout } from './default-layout';
-import { ConfigTheme, ConfigThemes, DefaultThemeType, PluginConfig } from './types';
+import { ConfigTheme, ConfigThemes, DefaultThemeType, HeroUIPluginConfig } from './types';
 import { utilities } from './utilities';
+import { DEFAULT_TRANSITION_DURATION } from './utilities/transition';
 import { baseStyles } from './utils/classes';
 import { flattenThemeObject } from './utils/object';
+import { isBaseTheme } from './utils/theme';
 
-/**
- * Determines if the theme is a base theme
- *
- * @param theme string
- * @returns "light" | "dark
- */
-const isBaseTheme = (theme: string) => theme === 'light' || theme === 'dark';
-const DEFAULT_PREFIX = 'aliceui';
+const DEFAULT_PREFIX = 'heroui';
+
 const parsedColorsCache: Record<string, number[]> = {};
 
 // @internal
@@ -141,6 +143,9 @@ const corePlugin = (
 ) => {
   const resolved = resolveConfig(themes, defaultTheme, prefix);
 
+  const createStripeGradient = (stripeColor: string, backgroundColor: string) =>
+    `linear-gradient(45deg,  hsl(var(--${prefix}-${stripeColor})) 25%,  hsl(var(--${prefix}-${backgroundColor})) 25%,  hsl(var(--${prefix}-${backgroundColor})) 50%,  hsl(var(--${prefix}-${stripeColor})) 50%,  hsl(var(--${prefix}-${stripeColor})) 75%,  hsl(var(--${prefix}-${backgroundColor})) 75%,  hsl(var(--${prefix}-${backgroundColor})))`;
+
   return plugin(
     ({ addBase, addUtilities, addVariant }) => {
       // add base classNames
@@ -164,21 +169,20 @@ const corePlugin = (
     {
       theme: {
         extend: {
+          // @ts-ignore
           colors: {
             ...(addCommonColors ? commonColors : {}),
             ...resolved?.colors,
+          },
+          scale: {
+            '80': '0.8',
+            '85': '0.85',
           },
           height: {
             divider: `var(--${prefix}-divider-weight)`,
           },
           width: {
             divider: `var(--${prefix}-divider-weight)`,
-          },
-          fontSize: {
-            tiny: [`var(--${prefix}-font-size-tiny)`, `var(--${prefix}-line-height-tiny)`],
-            small: [`var(--${prefix}-font-size-small)`, `var(--${prefix}-line-height-small)`],
-            medium: [`var(--${prefix}-font-size-medium)`, `var(--${prefix}-line-height-medium)`],
-            large: [`var(--${prefix}-font-size-large)`, `var(--${prefix}-line-height-large)`],
           },
           borderRadius: {
             small: `var(--${prefix}-radius-small)`,
@@ -189,14 +193,39 @@ const corePlugin = (
             hover: `var(--${prefix}-hover-opacity)`,
             disabled: `var(--${prefix}-disabled-opacity)`,
           },
+          borderWidth: {
+            small: `var(--${prefix}-border-width-small)`,
+            medium: `var(--${prefix}-border-width-medium)`,
+            large: `var(--${prefix}-border-width-large)`,
+            1: '1px',
+            1.5: '1.5px',
+            3: '3px',
+            5: '5px',
+          },
           boxShadow: {
             small: `var(--${prefix}-box-shadow-small)`,
             medium: `var(--${prefix}-box-shadow-medium)`,
             large: `var(--${prefix}-box-shadow-large)`,
           },
+          backgroundSize: {
+            'stripe-size': '1.25rem 1.25rem',
+          },
+          backgroundImage: {
+            'stripe-gradient-default': createStripeGradient('default-200', 'default-400'),
+            'stripe-gradient-primary': createStripeGradient('primary-200', 'primary'),
+            'stripe-gradient-secondary': createStripeGradient('secondary-200', 'secondary'),
+            'stripe-gradient-success': createStripeGradient('success-200', 'success'),
+            'stripe-gradient-warning': createStripeGradient('warning-200', 'warning'),
+            'stripe-gradient-danger': createStripeGradient('danger-200', 'danger'),
+          },
           transitionDuration: {
+            0: '0ms',
             250: '250ms',
             400: '400ms',
+            DEFAULT: DEFAULT_TRANSITION_DURATION,
+          },
+          transitionTimingFunction: {
+            'soft-spring': 'cubic-bezier(0.155, 1.105, 0.295, 1.12)',
           },
           ...animations,
         },
@@ -205,7 +234,7 @@ const corePlugin = (
   );
 };
 
-export const aliceui = (config: PluginConfig = {}): ReturnType<typeof plugin> => {
+export const heroui = (config: HeroUIPluginConfig = {}): ReturnType<typeof plugin> => {
   const {
     themes: themeObject = {},
     defaultTheme = 'light',
